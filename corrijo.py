19e98324398e84bd9ab4a4a3698360a2a0400da7
Rafael Jamur <rjamur@gmail.com>
2009-06-29 01:31:54 -0300

    adicionado um hifem na descricao

--- corrijo.py
+++ corrijo.py
@@ -6,34 +6,31 @@ writer = csv.writer(open('corrijido.csv', 'wb'))
 linha = pat.next()
  
 while True:
+  linhanova = ""
   t = linha[2]
-  result = re.match( r'\d{3}\.\d{3}', t)
+  result = re.match( r'\d{3}\.\d{3}', t) 
   if result:
     print result.group(0)
-    linhanova = linha
+    linhanova = linha # linhanova = linha a ser gravada no novo arquivo
   else:
-    writer.writerow( linha ) #se não for número de patrimônio grava como está
-    linha = pat.next()
-    continue
-  linha = pat.next()
-  result = re.match( r'\d{3}\.\d{3}', t)
-
+    writer.writerow( linha )  # se não for número de patrimônio grava como está
+    linha = pat.next()        # e volta para o while 
+    continue # vai para o while
+    
   # itera dentro do registro (até o próximo número de patrimônio)
-  sai_do_um = False # para continue do loop de fora
   linha = pat.next()
   while True:
-    t = linha[2]
-    result = re.match( r'\d{3}\.\d{3}', t)
-    result2 = re.match( r'\w', linha[0] )
-    if result or result2:
-      sai_do_um = True
-      break
+    t = linha[2] # lê a célula da coluna núm. de patrimônio
+    result = re.match( r'\d{3}\.\d{3}', t) # verifica é do padrão núm. patrim.
+    result2 = re.match( r'\w', linha[0] ) # se houver uma palavra na 1ª col. 
+    if result or result2: # se na próxima for um número de patrimônio
+      break               # termina esse laço (while)
     else:
-      # agora verifica se tem uma palavra no início da 'célula'
+      # agora verifica se tem a continuação da descrição do ítem, na segunda
+      # linha (só que fica uma coluna antes)
       if re.match( r'\w', t):
-        linhanova[3] += ' ' + linha[2]
-      linha = pat.next()
-  writer.writerow(linhanova)
-  if sai_do_um:
-    continue 
+        linhanova[3] += ' - ' + linha[2]
+      linha = pat.next() # lê próx. linha - termina o prog se não houver (raise)
 
+  writer.writerow(linhanova) # escreve a linha modificada no arquivo
+# termina o loop quando não houver mais linhas no arquivo
